65bf5cc0cf56fe5be819e4c151c8df46344dffcd
Mir Masudur Rhaman <mirmrhaman@gmail.com>
2021-09-07 01:08:52 -0400

    Merge remote-tracking branch 'origin/main' into main

--- src/test/java/page_object/NavigationBar.java
+++ src/test/java/page_object/NavigationBar.java
@@ -1,8 +1,10 @@
 package page_object;
 
+import command_providers.ActOn;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class NavigationBar {
     private static final Logger LOGGER = LogManager.getLogger(NavigationBar.class);
@@ -10,6 +12,20 @@ public class NavigationBar {
     private final By TargetLogo = By.xpath("//a[@id='home']");
 
 
+    public WebDriver driver;
+    public NavigationBar(WebDriver driver) {
+        this.driver = driver;
+    }
 
+
+    //Click on Categories
+
+
+    // Navigate to Home Page
+    public Home navigateToHome() {
+        LOGGER.debug("Navigating to Home Page");
+        ActOn.element(driver, TargetLogo).click();
+        return new Home(driver);
+    }
 
 }
